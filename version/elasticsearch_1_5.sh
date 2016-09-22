16c085b08004206c9236eb58d1ccc0ed3138a5f3
Abhijit Kini <abhijit@shippable.com>
2016-09-22 13:26:08 -0700

    Update elasticsearch to 2.4.0
    - https://www.elastic.co/guide/en/elasticsearch/reference/2.4/_installation.html

--- version/elasticsearch_1_5.sh
+++ version/elasticsearch_1_5.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-VERSION=1.5.1
+VERSION=2.4.0
 
-echo "================= Installing ElasticSearch ==================="
-sudo wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-${VERSION}.tar.gz
-sudo tar xzf elasticsearch-${VERSION}.tar.gz -C /usr/local && sudo rm -f elasticsearch-${VERSION}.tar.gz
-sudo ln -s /usr/local/elasticsearch-${VERSION}/bin/elasticsearch /usr/local/bin/elasticsearch
+echo "================= Installing ElasticSearch 2.4.0 ==================="
+curl -L -O https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.4.0/elasticsearch-2.4.0.tar.gz
+sudo tar -xvf elasticsearch-2.4.0.tar.gz -C /usr/local && sudo rm -f elasticsearch-2.4.0.tar.gz
+sudo ln -s /usr/local/elasticsearch-2.4.0/bin/elasticsearch /usr/local/bin/elasticsearch
